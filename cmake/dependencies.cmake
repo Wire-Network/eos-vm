3581e229b1fb5809e7a206f5e8bbb6552f6facd1
Jonathan Glanz <jonathan.glanz@gmail.com>
2026-02-08 13:47:20 -0500

    Updated dependency configs for llvm18 migration

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -11,4 +11,9 @@ file(COPY "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include/catch.hpp" DES
 
 # Softfloat
 find_package(Threads REQUIRED)
-find_package(softfloat CONFIG REQUIRED) 
+find_package(softfloat CONFIG REQUIRED)
+
+# LLVM
+find_package(LLVM CONFIG REQUIRED)
+message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
+message(STATUS "Using LLVMConfig.cmake at: ${LLVM_DIR}")
